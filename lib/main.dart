f552344458df545f3550b2f5ee635cfe615fa933
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-09-11 19:20:14 +0330

    Completed steps 1-4: Added interactivity and random number generation for ball images (0-5).

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
     MaterialApp(
-      home: null,
+      home: BallPage(),
     ),
   );
+}
+
+// Step 1 - Create a Stateless Widget
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Magic 8 Ball'),
+        backgroundColor: Colors.blue,
+      ),
+      backgroundColor: Colors.white,
+      body: Ball(),
+    );
+  }
+}
+
+// Step 2 - Create a Stateful Widget
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0; // Step 4 - Create a variable for ball number
+
+  void changeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(6); // Generates a random number between 0-5
+      print('ballNumber: $ballNumber');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          print('I got clicked'); // Step 3 - Print when clicked
+          changeBall(); // Step 4 - Randomise the ball image
+        },
+        child: Image.asset('images/ball$ballNumber.png'), // Step 5 - Display the ball image
+      ),
+    );
+  }
 }
